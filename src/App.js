c1573fd4c4173f42522348d723d1e6302ab12d48
alencarlucas <alencarlucaas@gmail.com>
2019-01-27 21:31:56 -0200

    feat: Use API to load data

--- src/App.js
+++ src/App.js
@@ -4,28 +4,86 @@ import SearchBooks from './Components/SearchBooks'
 import ListBooks from './Components/ListBooks'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
+import * as BooksAPI from './BooksAPI'
+
+const SHELVES = [
+  { value: 'currentlyReading', mask: 'Currently Reading' },
+  { value: 'wantToRead', mask: 'Want to Read' },
+  { value: 'read', mask: 'Read' },
+  { value: 'none', mask: 'None', visibleOnlyOnSearch: true }
+];
+
+const NO_QUERY_PARAMS_STRING = 'Type some text on input above to release a search';
+
+const NO_RESULTS_FOUND_STRING = 'No results found'
 
 class BooksApp extends React.Component {
   constructor() {
     super()
 
     this.state = {
-      /**
-       * TODO: Instead of using this state variable to keep track of which page
-       * we're on, use the URL in the browser's address bar. This will ensure that
-       * users can use the browser's back and forward buttons to navigate between
-       * pages, as well as provide a good URL they can bookmark and share.
-       */
-      showSearchPage: false
+      bookShelves: [],
+      loaded: false,
+      query: '',
+      searching: false,
+      foundBooks: [],
+      emptySearchTitle: NO_QUERY_PARAMS_STRING
     }
+  }
+
+  async componentDidMount() {
+    const results = await BooksAPI.getAll();
+    // Create a list of books grouped by shelf.
+    const bookShelves = SHELVES
+      .filter(shelf => !shelf.visibleOnlyOnSearch)
+      .map(shelf => {
+        return {
+          shelf: shelf.mask,
+          books: results.filter(book => book.shelf === shelf.value) || []
+        }
+      })
+    this.setState({ bookShelves, loaded: true });
+  }
+
+  async onSearch() {
+    const { query } = this.state.query;
+    const foundBooks = await BooksAPI.search(query)
+    this.setState({
+      foundBooks, 
+      searching: false,
+      emptySearchTitle: !foundBooks || foundBooks.error ? NO_RESULTS_FOUND_STRING :  ''
+    })
+  }
+
+  async onTextChange(text) {
+    const query = text ? { query: text } : ''
+    await this.setState({
+      query,
+      searching: query && true,
+      emptySearchTitle: !query ? NO_QUERY_PARAMS_STRING :  ''
+    })
+    // Realise a search when a value is passed to the search input
+    query && await this.onSearch()
   }
 
   render() {
     return (
       <div className="app">
-        <Route exact path='/' component={ListBooks} />
+        <Route exact path='/' render={() => (
+          <ListBooks
+            bookShelves={this.state.bookShelves}
+            loaded={this.state.loaded}
+            shelves={SHELVES}
+          />
+        )} />
         <Route path='/search' render={({ history }) => (
-          <SearchBooks onCloseSearch={() => history.push('/')} />
+          <SearchBooks
+            foundBooks={this.state.foundBooks}
+            emptySearchTitle={this.state.emptySearchTitle}
+            searching={this.state.searching}
+            shelves={SHELVES}
+            onCloseSearch={() => history.push('/')}
+            onTextChange={this.onTextChange.bind(this)}/>
         )} />
       </div>
     )
--- src/Components/SearchBooks.jsx
+++ src/Components/SearchBooks.jsx
@@ -1,29 +1,46 @@
 import React, { Component } from 'react';
+import AwesomeDebouncePromise from 'awesome-debounce-promise';
+import Loader from 'react-loader';
+import Book from './Book'
+import EmptyState from './EmptySate'
+
 
 class SearchBooks extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onHandleText = AwesomeDebouncePromise(this.props.onTextChange, 500);
+  }
+
   render() {
+    const { foundBooks, emptySearchTitle, shelves, searching, onCloseSearch } = this.props
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <button
             className="close-search"
-            onClick={this.props.onCloseSearch}>
+            onClick={onCloseSearch}>
               Close
           </button>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={(e) => this.onHandleText(e.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <Loader loaded={!searching}>
+            <ol className="books-grid">
+              {
+                foundBooks && foundBooks.length
+                  ? foundBooks.map(book => <Book shelves={shelves} {...book} key={book.id} />)
+                  : <EmptyState title={emptySearchTitle} />
+              }
+            </ol>
+          </Loader>
         </div>
       </div>
     )
